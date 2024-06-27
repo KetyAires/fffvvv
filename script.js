e4f84a6ee36dc9a77a58a629aa0c465b21fc7512
KetyAires <106245378+KetyAires@users.noreply.github.com>
2024-06-27 10:28:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -8,14 +6,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-       enunciado: "Qual é a sua ideia de um encontro perfeito?",
+        enunciado: "Qual é a sua ideia de um encontro perfeito?",
         alternativas: [
             {
-                texto: "Uma caminhada romântica pela floresta ao pôr do sol",
+                texto: "Uma caminhada romântica pela floresta ao pôr do sol!",
                 afirmacao: "Você é atraído (a) pelo calor e pela lealdade inabalável."
             },
             {
-                texto: "Um jantar à luz de velas em um restaurante elegante.",
+                texto: "Um jantar à luz de velas em um restaurante elegante!",
                 afirmacao: "Você é atraído(a) pelo mistério e pela elegância."
             }
         ]
@@ -24,13 +22,11 @@ const perguntas = [
         enunciado: "O que te atrai mais em uma pessoa?",
         alternativas: [
             {
-                texto: "Um espírito aventureiro e um físico
-atlético",
+                texto: "Um espírito aventureiro e um físico atlético.",
                 afirmacao: "Jacob representa aventura, parceria e um amor que é sempre protetor e apaixonado."
             },
             {
-                texto: "Um charme misterioso e uma
-inteligência aguçada",
+                texto: "Um charme misterioso e uma inteligência aguçada.",
                 afirmacao: "Edward oferece um amor eterno e uma ligação intelectual profunda."
             }
         ]
@@ -39,44 +35,42 @@ inteligência aguçada",
         enunciado: "Como você lida com situações difíceis?",
         alternativas: [
             {
-                texto: "Com paixão e determinação, nunca desistindo",
+                texto: "Com paixão e determinação, nunca desistindo.",
                 afirmacao: "Vocês dois compartilham uma conexão profunda com a natureza e uma energia vibrante."
             },
             {
-                texto: "Com calma e reflexão, pensando antes de agir",
-                afirmacao: "Vocês compartilham um gosto pelo clássico e pelo refinado, apreciando momentos tranquilos e reflexivos juntos."
-            }
-        ]
-    },
-    {
-        enunciado: "Como você se sente em relação ao sobrenatural?",
-        alternativas: [
-            {
-                texto: "Fascinado(a) e curioso (a), sempre aberto a) a novas possibilidades
- ",
+                texto: "Com calma e reflexão, pensando antes de agir.",
                 afirmacao: "Quer compreender tudo o que faz parte do universo, mas há uma área que costuma intrigá-lo particularmente: o sobrenatural."
-            },
-            {
-                texto: "Cético(a) mas interessado(a), gostando de explorar o desconhecido",
-                afirmacao: "Quer compreender tudo o que faz parte do universo, mas há uma área que costuma intrigá-lo particularmente: a natureza humana."
             }
         ]
     },
     {
-        enunciado: "Qual é o seu estilo de vida?",
+        enunciado: "Qual é a sua estação do ano favorita?",
         alternativas: [
             {
-                texto: "Ativo (a) e cheio a) de energia",
-                afirmacao: "Você vai casar com Jacob, o Lobo!"
+                texto: "Verão, para aproveitar o calor e a vida ao ar livre.",
+                afirmacao: "Quer compreender tudo o que faz parte do universo, mas há uma área que costuma intrigá-lo particularmente: a natureza humana."
             },
             {
-                texto: "Tranquilo (a) e introspectivo (a)",
-                afirmacao: "Você vai casar com *Edward, o Vampiro*!"
+                texto: "Inverno, para apreciar o frio e o aconchego.",
+                afirmacao: ","
+            }
+        ]
+    },
+    {
+        enunciado: "Como você se sente em relação ao sobrenatural? ",
+        alternativas: [
+            {
+                texto: "Fascinado(a) e curioso(a), sempre aberto(a) a novas possibilidades.",
+                afirmacao: "Você vai casar com **Jacob, o Lobo**!  "
+            },
+            {
+                texto: "Cético(a) mas interessado(a), gostando de explorar o desconhecido.",
+                afirmacao: "Você vai casar com **Edward, o Vampiro**! "
             }
         ]
     },
 ];
-                
 
 
 let atual = 0;
